1fd071a2baf569a1f46d18fafd8e81077464121c
Hristo Dzhambov <dzhambov@hotmail.com>
2020-04-24 23:56:21 -0400

    add props and propTypes.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Item(props){
   return (
     <React.Fragment>
-      <h3>3a</h3>
-      <h3>{props.name} and {props.brand}</h3>
-      <p><em>Firebase entries not saving!</em></p>
+      <h3>{props.name}</h3>
+      <h4>Brand:{props.brand}</h4>
+      <h4>Flavor:{props.flavor}</h4>
+      <h4>Price:{props.price}</h4>
+      <h5>Quantity:{props.quantity}</h5>
       <hr/>
     </React.Fragment>
   );
+}
+
+Item.propTypes = {
+  names: PropTypes.string,
+  brand: PropTypes.string,
+  flavor: PropTypes.string,
 }
 
 export default Item;
