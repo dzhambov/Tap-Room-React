0ca9ac43a52704aa934600358ae0a380a5150d52
Hristo Dzhambov <dzhambov@hotmail.com>
2020-04-25 00:15:21 -0400

    add looping with map() method.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,36 @@
 import React from "react";
 import Item from "./Item";
 
+const masterItemList = [
+  {
+    name: "Beer",
+    brand: "Heineken",
+    flavor: "Refreshing",
+    price: "$6",
+    quantity: "124"
+  },
+  {
+    name: "Kombucha",
+    brand: "Bao",
+    flavor: "Refreshing",
+    price: "$4",
+    quantity: "124"
+  }
+];
+
+
 function ItemList(){
   return (
     <React.Fragment>
-      <Item 
-      name="Beer"
-      brand="Heineken"/>
-
+      <hr/>
+      {masterItemList.map((item, itemId) =>
+      <Item name={item.name}
+            brand={item.brand}
+            flavor={item.flavor}
+            price={item.price}
+            quantity={item.quantity}
+            key={itemId}/>
+      )}
     </React.Fragment>
   );
 }
