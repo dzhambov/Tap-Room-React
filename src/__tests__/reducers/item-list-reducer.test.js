b424559af6e99ac5dfe5ca40ff57878ebdcaa3a0
Hristo Dzhambov <dzhambov@hotmail.com>
2020-05-08 16:00:08 -0700

    add a third test that fails and passes as expected.

--- src/__tests__/reducers/item-list-reducer.test.js
+++ src/__tests__/reducers/item-list-reducer.test.js
@@ -3,6 +3,22 @@ import itemListReducer from '../../reducers/item-list-reducer';
 describe('itemListReducer', () => {
 
   let action;
+
+  const currentState = {
+    1: { name: 'Beer',
+    brand: 'Sam Adams',
+    flavor: 'lager',
+    price: '$6',
+    quantity: '9',
+    id: 1 },
+    2: { name: 'Kombucha',
+    brand: 'BAO',
+    flavor: 'bluberry',
+    price: '$4',
+    quantity: '3',
+    id: 2 }
+  }
+
   const itemData = {
     name: 'Beer',
     brand: 'Sam Adams',
@@ -28,14 +44,29 @@ describe('itemListReducer', () => {
       id: id
     };
 
-    expect(itemListReducer({}, action)).toEqual({[id]: {
-      name: name,
-      brand:brand,
-      flavor: flavor,
-      price: price,
-      quantity: quantity,
-      id: id
-    }
+    expect(itemListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand:brand,
+        flavor: flavor,
+        price: price,
+        quantity: quantity,
+        id: id
+      }
+    });
   });
+  test('Should successfully delete an item', () => {
+    action = {
+      type: 'DELETE_ITEM',
+      id: 1
+    };
+    expect(itemListReducer(currentState, action)).toEqual({
+      2: {  name: 'Kombucha',
+      brand: 'BAO',
+      flavor: 'bluberry',
+      price: '$4',
+      quantity: '3',
+      id: 2 }
+    });
   });
 });
